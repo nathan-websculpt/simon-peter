66e93e218e8dc0ab2a96fe1cac902f36cf85a888
nathan-websculpt <websculpt.nathan@gmail.com>
2024-08-30 11:15:27 -0500

    config change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+};
 
 module.exports = nextConfig;
