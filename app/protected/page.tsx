7e40fe7b4158950c3b5825e6746d8b7bd8e77067
nathan-websculpt <websculpt.nathan@gmail.com>
2024-08-30 11:06:11 -0500

    build err fix

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,5 @@
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
@@ -33,7 +32,6 @@ export default async function ProtectedPage() {
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-          <FetchDataSteps />
         </main>
       </div>
 
