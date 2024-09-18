281c13006b80518bc56508e26e4dbeff7270d06a
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-18 17:50:34 -0500

    using IntersectionObserver to show more imgs as scrolling down

--- components/views/Summaries.tsx
+++ components/views/Summaries.tsx
@@ -9,11 +9,25 @@ export function Summaries() {
   const [firstRun, setFirstRun] = useState(true);
   const elemRefOne = useRef(null);
   const elemRefTwo = useRef(null);
+  const elemRefThree = useRef(null);
+  const elemRefFour = useRef(null);
+  const elemRefFive = useRef(null);
+  const elemRefSix = useRef(null);
+
+  const bottomRef = useRef(null);
 
   const [showSectionThree, setShowSectionThree] = useState(false);
+  const [showSectionFour, setShowSectionFour] = useState(false);
+  const [showSectionFive, setShowSectionFive] = useState(false);
+  const [showSectionSix, setShowSectionSix] = useState(false);
+  const [showSectionSeven, setShowSectionSeven] = useState(false);
+  const [showSectionEight, setShowSectionEight] = useState(false);
+  const options = { root: null, rootMargin: "0px", threshold: 1.0 };
 
-  const callbackFunction = (entries) => {
-    console.log("callbackFunction called with following entries:", entries);
+  // BEGIN::
+  // elemRefOne IntersectionObserver
+  const elemRefOneCallback = (entries) => {
+    console.log("elemRefOneCallback called with following entries:", entries);
     const [entry] = entries;
     console.log("entry.isIntersecting:", entry.isIntersecting);
     if (entry.isIntersecting) {
@@ -21,18 +35,15 @@ export function Summaries() {
     }
   };
 
-  const options = { root: null, rootMargin: "0px", threshold: 1.0 };
-
   useEffect(() => {
     if (firstRun) {
       setFirstRun(false);
       return;
     }
-    console.log("useEffect called");
-    const observer = new IntersectionObserver(callbackFunction, options);
+    const observer = new IntersectionObserver(elemRefOneCallback, options);
     if (elemRefOne.current) {
       console.log(
-        "Observing the following element with IntersectionObserver",
+        "Observing the elemRefOne element with IntersectionObserver",
         elemRefOne.current
       );
       observer.observe(elemRefOne.current);
@@ -41,13 +52,250 @@ export function Summaries() {
     return () => {
       if (elemRefOne.current) {
         console.log(
-          "Unobserving the following element with IntersectionObserver",
+          "Unobserving the elemRefOne element with IntersectionObserver",
           elemRefOne.current
         );
         observer.unobserve(elemRefOne.current);
       }
     };
   }, [options, elemRefOne]);
+  //END: elemRefOne IntersectionObserver
+  // END::
+
+  // BEGIN::
+  // elemRefTwo IntersectionObserver
+  const elemRefTwoCallback = (entries) => {
+    console.log("elemRefTwoCallback called with following entries:", entries);
+    const [entry] = entries;
+    console.log("entry.isIntersecting:", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      setShowSectionFour(true);
+    }
+  };
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      return;
+    }
+    const observer = new IntersectionObserver(elemRefTwoCallback, options);
+    if (elemRefTwo.current) {
+      console.log(
+        "Observing the elemRefTwo element with IntersectionObserver",
+        elemRefTwo.current
+      );
+      observer.observe(elemRefTwo.current);
+    }
+
+    return () => {
+      if (elemRefTwo.current) {
+        console.log(
+          "Unobserving the elemRefTwo element with IntersectionObserver",
+          elemRefTwo.current
+        );
+        observer.unobserve(elemRefTwo.current);
+      }
+    };
+  }, [options, elemRefTwo]);
+  //END: elemRefTwo IntersectionObserver
+  // END::
+
+  // BEGIN::
+  // elemRefThree IntersectionObserver
+  const elemRefThreeCallback = (entries) => {
+    console.log("elemRefThreeCallback called with following entries:", entries);
+    const [entry] = entries;
+    console.log("entry.isIntersecting:", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      setShowSectionFive(true);
+    }
+  };
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      return;
+    }
+    const observer = new IntersectionObserver(elemRefThreeCallback, options);
+    if (elemRefThree.current) {
+      console.log(
+        "Observing the elemRefThree element with IntersectionObserver",
+        elemRefThree.current
+      );
+      observer.observe(elemRefThree.current);
+    }
+
+    return () => {
+      if (elemRefThree.current) {
+        console.log(
+          "Unobserving the elemRefThree element with IntersectionObserver",
+          elemRefThree.current
+        );
+        observer.unobserve(elemRefThree.current);
+      }
+    };
+  }, [options, elemRefThree]);
+  //END: elemRefThree IntersectionObserver
+  // END::
+
+  // BEGIN::
+  // elemRefFour IntersectionObserver
+  const elemRefFourCallback = (entries) => {
+    console.log("elemRefFourCallback called with following entries:", entries);
+    const [entry] = entries;
+    console.log("entry.isIntersecting:", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      setShowSectionSix(true);
+    }
+  };
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      return;
+    }
+    const observer = new IntersectionObserver(elemRefFourCallback, options);
+    if (elemRefFour.current) {
+      console.log(
+        "Observing the elemRefFour element with IntersectionObserver",
+        elemRefFour.current
+      );
+      observer.observe(elemRefFour.current);
+    }
+
+    return () => {
+      if (elemRefFour.current) {
+        console.log(
+          "Unobserving the elemRefFour element with IntersectionObserver",
+          elemRefFour.current
+        );
+        observer.unobserve(elemRefFour.current);
+      }
+    };
+  }, [options, elemRefFour]);
+  //END: elemRefFour IntersectionObserver
+  // END::
+
+  // BEGIN::
+  // elemRefFive IntersectionObserver
+  const elemRefFiveCallback = (entries) => {
+    console.log("elemRefFiveCallback called with following entries:", entries);
+    const [entry] = entries;
+    console.log("entry.isIntersecting:", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      setShowSectionSeven(true);
+    }
+  };
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      return;
+    }
+    const observer = new IntersectionObserver(elemRefFiveCallback, options);
+    if (elemRefFive.current) {
+      console.log(
+        "Observing the elemRefFive element with IntersectionObserver",
+        elemRefFive.current
+      );
+      observer.observe(elemRefFive.current);
+    }
+
+    return () => {
+      if (elemRefFive.current) {
+        console.log(
+          "Unobserving the elemRefFive element with IntersectionObserver",
+          elemRefFive.current
+        );
+        observer.unobserve(elemRefFive.current);
+      }
+    };
+  }, [options, elemRefFive]);
+  //END: elemRefFive IntersectionObserver
+  // END::
+
+  // BEGIN::
+  // elemRefSix IntersectionObserver
+  const elemRefSixCallback = (entries) => {
+    console.log("elemRefSixCallback called with following entries:", entries);
+    const [entry] = entries;
+    console.log("entry.isIntersecting:", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      setShowSectionEight(true);
+    }
+  };
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      return;
+    }
+    const observer = new IntersectionObserver(elemRefSixCallback, options);
+    if (elemRefSix.current) {
+      console.log(
+        "Observing the elemRefSix element with IntersectionObserver",
+        elemRefSix.current
+      );
+      observer.observe(elemRefSix.current);
+    }
+
+    return () => {
+      if (elemRefSix.current) {
+        console.log(
+          "Unobserving the elemRefSix element with IntersectionObserver",
+          elemRefSix.current
+        );
+        observer.unobserve(elemRefSix.current);
+      }
+    };
+  }, [options, elemRefSix]);
+  //END: elemRefSix IntersectionObserver
+  // END::
+
+  // BEGIN::
+  // bottomRef IntersectionObserver -- will open bottom section
+  const bottomRefCallback = (entries) => {
+    console.log("bottomRefCallback called with following entries:", entries);
+    const [entry] = entries;
+    console.log("entry.isIntersecting:", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      // setShowSectionThree(true);
+      // setShowSectionFour(true);
+      // setShowSectionFive(true);
+      //setShowSectionSix(true);
+      //TODO: be sure all sections are visible
+      //TODO: prevent CTRL + END from causing them to hide when scroll up
+      //TODO: prevent CTRL + HOME from causing them to hide when scroll down
+      //TODO: having problems with fast-scrolling past refs
+    }
+  };
+
+  useEffect(() => {
+    if (firstRun) {
+      setFirstRun(false);
+      return;
+    }
+    const observer = new IntersectionObserver(bottomRefCallback, options);
+    if (bottomRef.current) {
+      console.log(
+        "Observing the bottomRef element with IntersectionObserver",
+        bottomRef.current
+      );
+      observer.observe(bottomRef.current);
+    }
+
+    return () => {
+      if (bottomRef.current) {
+        console.log(
+          "Unobserving the bottomRef element with IntersectionObserver",
+          bottomRef.current
+        );
+        observer.unobserve(bottomRef.current);
+      }
+    };
+  }, [options, bottomRef]);
+  //END: bottomRef IntersectionObserver
+  // END::
 
   return (
     <>
@@ -76,7 +324,10 @@ export function Summaries() {
         <>
           {/* BOOKS OF Poetry */}
           <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
-            <h1 className="prose prose-2xl underline underline-offset-8">
+            <h1
+              className="prose prose-2xl underline underline-offset-8"
+              ref={elemRefTwo}
+            >
               Books of Poetry
             </h1>
 
@@ -84,6 +335,111 @@ export function Summaries() {
           </div>
         </>
       )}
+
+      {showSectionFour && (
+        <>
+          {/* BOOKS OF Major Prophets */}
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1
+              className="prose prose-2xl underline underline-offset-8"
+              ref={elemRefThree}
+            >
+              Books of Major Prophets
+            </h1>
+
+            <ImgLoop len={5} offset={23} lastImg={27} />
+          </div>
+        </>
+      )}
+
+      {showSectionFive && (
+        <>
+          {/* BOOKS OF Minor Prophets */}
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1
+              className="prose prose-2xl underline underline-offset-8"
+              ref={elemRefFour}
+            >
+              Books of Minor Prophets
+            </h1>
+
+            <ImgLoop len={12} offset={28} lastImg={39} />
+          </div>
+        </>
+      )}
+
+      {showSectionSix && (
+        <>
+          {/* GOSPELS + ACTS */}
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1
+              className="prose prose-2xl underline underline-offset-8"
+              ref={elemRefFive}
+            >
+              Gospels
+            </h1>
+
+            <ImgLoop len={4} offset={40} lastImg={43} />
+          </div>
+
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1 className="prose prose-2xl underline underline-offset-8">
+              History of the early Church
+            </h1>
+
+            <img
+              className="mx-auto lg:w-4/5 xl:w-3/5 my-4 xl:my-12"
+              src="/img/bible_summary_dark/44.png"
+              alt="The Bible Summarized – Book 44"
+            />
+          </div>
+        </>
+      )}
+
+      {showSectionSeven && (
+        <>
+          {/* PAULINE EPISTLES */}
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1
+              className="prose prose-2xl underline underline-offset-8"
+              ref={elemRefSix}
+            >
+              Pauline Epistles
+            </h1>
+
+            <ImgLoop len={13} offset={45} lastImg={57} />
+          </div>
+        </>
+      )}
+
+      {showSectionEight && (
+        <>
+          {/* GENERAL EPISTLES */}
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1 className="prose prose-2xl underline underline-offset-8">
+              General Epistles
+            </h1>
+
+            <ImgLoop len={8} offset={58} lastImg={65} />
+          </div>
+
+          {/* REVELATION */}
+          <div className="w-full flex flex-col gap-2 items-center mt-5 xl:mt-12 border-t pt-6 xl:pt-16">
+            <h1 className="prose prose-2xl underline underline-offset-8">
+              The Revelation of Jesus Christ
+            </h1>
+
+            <img
+              className="mx-auto lg:w-4/5 xl:w-3/5 my-4 xl:my-12"
+              src="/img/bible_summary_dark/66.png"
+              alt="The Bible Summarized – Book 66"
+            />
+          </div>
+        </>
+      )}
+
+      {/* TODO: add bottom ref */}
+      {/* <div ref={bottomRef}></div> */}
     </>
   );
 }
