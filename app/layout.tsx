b6920f334f6b588e4957fc488d860fda00257879
nathan-websculpt <websculpt.nathan@gmail.com>
2024-09-19 14:56:36 -0500

    vercel env messed up og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+// const defaultUrl = process.env.VERCEL_URL
+//   ? `https://${process.env.VERCEL_URL}`
+//   : "http://localhost:3000";
+
+const defaultUrl = "https://thekjvbible.vercel.app";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
